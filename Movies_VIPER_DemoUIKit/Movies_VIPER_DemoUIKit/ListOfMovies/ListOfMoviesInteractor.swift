4d389cfb7aec3534e4a01dbcde3563e04b0c0398
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-10-01 00:22:44 -0500

    interactor configuración, solicitud http usando el apikey de cuenta themoviedb

--- Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesInteractor.swift
+++ Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesInteractor.swift
@@ -6,3 +6,11 @@
 //
 
 import Foundation
+
+class ListOfMoviesInteractor {
+    func getMovies() async -> PopularMovieResponseEntity {
+        let url = URL(string: "https://api.themoviedb.org/3/movie/popular?api_key=e9fafd40b66830e9290f5a8f8eaea9d3")!
+        let (data, _) = try! await URLSession.shared.data(from: url)
+        return try! JSONDecoder().decode(PopularMovieResponseEntity.self, from: data)
+    }
+}
