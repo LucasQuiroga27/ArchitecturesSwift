3564b92dc9ad7e74d45e7210576892948bd3cb70
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-09-30 23:00:26 -0500

    creando proyecto para aplicar VIPER

---
+++ Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Movies_VIPER_DemoUIKit
+//
+//  Created by Mac Home on 30/09/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
