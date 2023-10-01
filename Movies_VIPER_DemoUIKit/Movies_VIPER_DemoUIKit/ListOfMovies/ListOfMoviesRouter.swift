f5735d27d7cdce22da70e1339dba3fd588c82d61
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-10-01 16:58:55 -0500

    configurando router sin storyBoard

--- Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesRouter.swift
+++ Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesRouter.swift
@@ -3,6 +3,22 @@
 //  Movies_VIPER_DemoUIKit
 //
 //  Created by Mac Home on 30/09/23.
-//  e9fafd40b66830e9290f5a8f8eaea9d3
+//
 
 import Foundation
+import UIKit
+
+class ListOfMoviesRouter {
+    
+    func showListOfMovies(window: UIWindow?){
+        let view = ListOfMoviesView()
+        let interactor = ListOfMoviesInteractor()
+        let presenter = ListOfMoviesPresenter(listOfMoviesInteractor: interactor)
+        presenter.ui = view
+        view.presenter = presenter
+        
+        window?.rootViewController = view
+        window?.makeKeyAndVisible()
+    }
+    
+}
