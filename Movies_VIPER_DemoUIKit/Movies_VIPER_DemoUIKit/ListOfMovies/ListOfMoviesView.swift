476925656cfb708e130f703f421b89d1d731ae3a
LucasQuiroga27 <lucasquirogafp@gmail.com>
2023-10-01 16:37:44 -0500

    creando referencia al presenter

--- Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesView.swift
+++ Movies_VIPER_DemoUIKit/Movies_VIPER_DemoUIKit/ListOfMovies/ListOfMoviesView.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+import UIKit
+
+class ListOfMoviesView: UIViewController {
+    
+    var presenter: ListOfMoviesPresenter?
+    
+    init(){
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder){
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemGray3
+        presenter?.onViewAppear()
+    }
+    
+}
+
+extension ListOfMoviesView: ListOfMoviesUI {
+    func update(movies: [PopularMovieEntity]) {
+        print("datos request : \(movies)")
+    }
+}
